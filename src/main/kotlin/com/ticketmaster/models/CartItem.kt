7cbaf9aadaa16ca31b9ea4d091cd8120fb93ad29
elonmusk96435-wq <elonmusk96435@gmail.com>
2026-02-17 10:15:07 +0100

    Create CartItem.kt model

--- src/main/kotlin/com/ticketmaster/models/CartItem.kt
+++ src/main/kotlin/com/ticketmaster/models/CartItem.kt
@@ -2,7 +2,7 @@ package com.ticketmaster.models
 
 data class CartItem(
     val ticket: Ticket,
-    var quantity: Int = 1
+    var quantity: Int
 ) {
     val subtotal: Double
         get() = ticket.pricePerTicket * quantity
