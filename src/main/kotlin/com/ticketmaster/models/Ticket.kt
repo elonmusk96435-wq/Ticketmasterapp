ae63e145888ef34c6ecebfcfd9d1f850e7a16252
elonmusk96435-wq <elonmusk96435@gmail.com>
2026-02-17 09:56:30 +0100

    Add Ticket.kt model for Ticketmaster app

---
+++ src/main/kotlin/com/ticketmaster/models/Ticket.kt
@@ -0,0 +1,14 @@
+package com.ticketmaster.models
+
+data class Ticket(
+    val id: String,
+    val eventId: String,
+    val eventTitle: String,
+    val quantity: Int,
+    val pricePerTicket: Double,
+    val date: String,
+    val location: String
+) {
+    val totalPrice: Double
+        get() = quantity * pricePerTicket
+}
