66d42897df654949f1c505b679172419b063b1e5
elonmusk96435-wq <elonmusk96435@gmail.com>
2026-02-17 10:37:02 +0100

    Create Event.kt file with Event data class

--- src/main/kotlin/com/ticketmaster/models/Event.kt
+++ src/main/kotlin/com/ticketmaster/models/Event.kt
@@ -3,11 +3,9 @@ package com.ticketmaster.models
 data class Event(
     val id: String,
     val name: String,
+    val artist: String,
     val date: String,
     val location: String,
-    val availableTickets: Int,
-    val description: String = "",
-    val imageUrl: String = "",
-    val venue: String = "",
-    val artist: String = ""
+    val price: Double,
+    val availableTickets: Int
 )
